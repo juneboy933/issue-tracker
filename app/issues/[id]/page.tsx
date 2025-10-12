6eead08ce91d43ec12af696d780af11a9996687b
Brian Oduor <brianoduor03@gmail.com>
2025-10-12 14:46:21 +0300

    Updated the [id]/page.tsx

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,34 +1,33 @@
 import { getCollection } from '@/lib/mongodb';
 import { ObjectId } from 'mongodb';
-import Link from 'next/link';
-import React from 'react'
+import React from 'react';
 import IssueActions from '@/app/components/IssueAction';
+
 interface IssuesPageProps {
-    params: {id: string};
+  params: { id: string };
 }
 
-const IssuePage = async({params}: IssuesPageProps) => {
-    const { id } = params;
-    const issueCollection = await getCollection("issues");
-    const issue = await issueCollection?.findOne({_id: new ObjectId(id)});
+const IssuePage = async ({ params }: IssuesPageProps) => {
+  const { id } = params;
+  const issueCollection = await getCollection('issues');
+  const issue = await issueCollection?.findOne({ _id: new ObjectId(id) });
 
-    if(!issue) {
-        return <h1 className='text-red-500'> Issues not found</h1>;
-    }
+  if (!issue) {
+    return <h1 className="text-red-500">Issue not found</h1>;
+  }
 
   return (
-    <div>
-        <h1 className='text-3xl font-bold mb-2'>{issue.title}</h1>
-        <p className='text-gray-600 text-lg'>{issue.description}</p>
-        <p className='text-gray-500'>{new Date(issue.createdAt).toLocaleString()}</p>
-        <Link 
-            href={`/issues/${issue._id}/edit`}
-            className='text-blue-600 hover:underline'>
-                Edit Issue
-        </Link>
-        <IssueActions id={id}/>
-    </div>
-  )
-}
+    <div className="p-6">
+      <h1 className="text-3xl font-bold mb-2">{issue.title}</h1>
+      <p className="text-gray-600 text-lg">{issue.description}</p>
+      <p className="text-gray-500 mb-4">
+        {new Date(issue.createdAt).toLocaleString()}
+      </p>
 
-export default IssuePage
+      {/* Action buttons */}
+      <IssueActions id={id} />
+    </div>
+  );
+};
+
+export default IssuePage;
